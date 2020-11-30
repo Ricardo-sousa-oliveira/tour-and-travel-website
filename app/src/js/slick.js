9895b877fa8ce6e0caf2d14df4a645198c05e111
Ricardo <ricardo.br.pi@gmail.com>
2020-11-30 00:09:32 -0300

    refactor(opinions__button) refactor opinions buttons to be functional

--- app/src/js/slick.js
+++ app/src/js/slick.js
@@ -1,7 +1,17 @@
 $(function() {
   $('.opinions__slick').slick({
+    dots: true,
     infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+    
   });
+  $('.opinions__button--next').on('click', function() {
+    $('.opinions__slick').slick('slickNext');
+  });
+  $('.opinions__button--back').on('click', function() {
+    $('.opinions__slick').slick('slickPrev');
+  });
+
 });
