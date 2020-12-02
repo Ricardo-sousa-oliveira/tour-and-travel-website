b2238582517c0bb0114772c9eb93848d18bd1987
Ricardo <ricardo.br.pi@gmail.com>
2020-12-01 23:11:14 -0300

    refactor(nav__hamburger) refactor the hamburger menu

--- app/src/js/hamburger.js
+++ app/src/js/hamburger.js
@@ -1,6 +1,6 @@
 $(function() {
   console.log( "ready!" );
-  $('.hamburger--arrowturn-r').on('click', function(){
+  $('.hamburger--stand').on('click', function(){
     $(this).toggleClass('is-active');
     $(this).parent().find('nav').slideToggle();
     
